f8066b2bb2127cd11c31bd3debd0a6c2fdf8e5a9
Tsneemh10 <68311792+Tsneemh10@users.noreply.github.com>
2023-05-02 15:46:12 +0300

    Splitting the channels and applied dilation, median filter, then appending them to remove shadows.

--- main.py
+++ main.py
@@ -33,19 +33,34 @@ if __name__ == '__main__':
         # read the path of the image, then assign it to the "imread function"
         image = cv2.imread(struct['filename'])
 
+        kernel_smoothing = np.ones((3, 3), np.float32) / 9
+        img_smoothed = cv2.filter2D(image, -1, kernel_smoothing)
+
         # convert the image to grayscale format
         img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        rgb_planes = cv2.split(img_gray)
+
+        result_planes = []
+        result_norm_planes = []
+        for plane in rgb_planes:
+            dilated_img = cv2.dilate(plane, np.ones((7, 7), np.uint8))
+            bg_img = cv2.medianBlur(dilated_img, 21)
+            diff_img = 255 - cv2.absdiff(plane, bg_img)
+            norm_img = cv2.normalize(diff_img, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8UC1)
+            result_planes.append(diff_img)
+            result_norm_planes.append(norm_img)
+
+        result = cv2.merge(result_planes)
+        result_norm = cv2.merge(result_norm_planes)
 
         # Apply histogram equalization to enhance the contrast of the image
         img_equalized = cv2.equalizeHist(img_gray)
 
         # Apply a median filter with a kernel size of 3
-        img_median = cv2.fastNlMeansDenoising(img_equalized)
+        img_median = cv2.fastNlMeansDenoising(result_norm)
 
-        kernel_smoothing = np.ones((9, 9), np.float32) / 81
-        img_smoothed = cv2.filter2D(img_gray, -1, kernel_smoothing)
-
-        ret, thresh = cv2.threshold(img_median, 125, 255, cv2.THRESH_BINARY)
+        ret, thresh = cv2.threshold(result_norm, 200, 255, cv2.THRESH_BINARY)
 
         # detect the contours on the binary image using cv2.CHAIN_APPROX_NONE
         contours, hierarchy = cv2.findContours(image=thresh, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_SIMPLE)
@@ -71,10 +86,10 @@ if __name__ == '__main__':
             cv2.rectangle(image, start, end, box_color, 1)
         """
 
-        window_name = 'image'
-        window_name2 = 'smoothed'
+        window_name = 'result_norm'
+        window_name2 = 'result'
 
-        cv2.imshow(window_name2, img_median)
+        cv2.imshow(window_name2, thresh)
 
         cv2.imshow(window_name, img_copy)
 
