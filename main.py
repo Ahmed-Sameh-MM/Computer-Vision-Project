1aafb3ad7747d019e308c7e7910aa799372f26fe
TeraByte256 <tbyte256@gmail.com>
2023-05-19 17:04:29 +0300

    Finalized the notebook again, added the Project Report PDF

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ if __name__ == '__main__':
     # call read_images_from_json to get the list of image objects
     training_images = RealData().read_images_from_json(file_name='training.json')
 
-    Localize().localize_digits(images=training_images, number_of_images=100, show_images=True)
+    Localize().localize_digits(images=training_images, number_of_images=500, show_images=True)
 
-    Recognize().test_images(images=training_images, number_of_images=100, show_images=True)
+    Recognize().test_images(images=training_images, number_of_images=500, show_images=True)
